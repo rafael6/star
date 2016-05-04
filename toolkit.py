5f780fd02c6826c0bffdab4b7cb9c2831392b846
rafael6 <rafael.gm01@gmail.com>
2016-05-03 20:12:29 -0400

    Update toolkit.py

--- toolkit.py
+++ toolkit.py
@@ -1,129 +1,159 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-__author__ = 'Rafael'
+__author__ = 'rafael'
+__version__ = '4.1.0'
 
-import paramiko
-import re
+"""
+This modules provides methods for checking: DNS, ICMP, sockets, and URL status.
+
+>>> import toolkit
+>>> hostname = 'yahoo.com'
+>>> print(toolkit.check_dns(hostname, ['8.8.8.8'], 'a'))
+206.190.36.45, 98.139.183.24, 98.138.253.109
+>>> print(toolkit.check_ping(hostname, '4'))
+--- yahoo.com ping statistics ---
+4 packets transmitted, 4 received, 0% packet loss, time 602ms
+rtt min/avg/max/mdev = 100.700/103.932/111.040/4.159 ms
+>>> print(toolkit.check_socket(hostname, 80, 'tcp'))
+open
+>>> print(toolkit.check_url('https://yahoo.com'))
+200
+"""
+
+import dns.resolver
 import socket
 import subprocess
-import telnetlib
-import urllib2
+import urllib.request
 
 
-class Toolkit(object):
+def check_dns(node, nservers, qtype):
     """
-    Provides the following methods:
-        check_host() for DNS resolution and ICMP stats.
-        check_socket() for socket/service status checking.
-        check_url() for URL status code checking.
+    Process name resolution (DNS) queries on a given node (hostname or IP)
+    using a name server from a given list of name servers (nservers), and a
+    query type (qtype). Returns DNS data otherwise an exception.
+
+    Example:
+        import toolkit
+        print(toolkit.check_dns(yahoo.com, ['8.8.8.8'], 'a'))
+
+    :param node: string, IP address or hostname in DNS.
+    :param nservers: list of strings, DNS servers.
+    :param qtype: string, one of the these query types: A/CNAME, MX, PTR.
+    :return: string, name resolution data otherwise an exception.
     """
-
-    def __init__(self, name, port=None, username=None, password=None):
-        self.name = name
-        self.port = port
-        self.username = username
-        self.password = password
-
-    def check_host(self):
-        """Ping a given hostname (argument) with a load of 1450 data bytes four
-        times and returns DNS resolution and ICMP-related information such as
-        packet and latency counters."""
-        command = 'ping -c 4 -s 1450 ' + self.name
-        proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-        output = proc.communicate()
-        match = re.search(r'---(.*)', output[0], re.DOTALL)
-
-        try:
-            return match.group()
-        except AttributeError:
-            return '\n!!!Unable to resolve %s; check its DNS!\n' % self.name
-
-    def check_socket(self):
-        """Check status of a given socket (hostname and port) (arguments) and
-        returns its connectivity status."""
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        try:
-            sock.connect((self.name, self.port))
-        except socket.error:
-            return '!!!Unable to connect to %s on port %s.' % (self.name, self.port)
+    try:
+        _resolver = dns.resolver.Resolver()
+        _resolver.nameservers = nservers
+        if qtype.lower() == 'mx':
+            _answer = _resolver.query(node, qtype)
+            _data = ['Host {} preferance {}'.format(
+                rdata.exchange, rdata.preference) for rdata in _answer]
+        elif qtype.lower() == 'ptr':
+            _answer = _resolver.query(dns.reversename.from_address(node), qtype)
+            _data = [str(record) for record in _answer]
         else:
-            return 'Port %s is open on %s.' % (self.port, self.name)
-        finally:
-            sock.close()
-
-    def check_url(self):
-        """Check the status of a given URL (argument) and returns its URL
-        status code."""
-        try:
-            connection = urllib2.urlopen(self.name)
-            code = connection.getcode()
-        except urllib2.HTTPError as e:
-            return '!!!There is an http problem with %s. s%' % (self.name, e)
-        except urllib2.URLError as e:
-            return '!!!There is an URL problem with %s. s%' % (self.name, e)
-        except Exception as e:
-            return e
-        else:
-            return 'URL %s returned a code of %s.' % (self.name, code)
+            # Handles both A and CNAME records
+            #_answer = _resolver.query(node, qtype)
+            _data = [str(address) for address in _resolver.query(node, qtype)]
+    except dns.exception.SyntaxError:
+        return 'Error; check IP address.'
+    except dns.rdatatype.UnknownRdatatype:
+        return 'Error; check query type.'
+    except dns.resolver.NoAnswer:
+        return 'Error; check query type.'
+    except dns.resolver.NXDOMAIN:
+        return 'Error; check hostname.'
+    except dns.exception.Timeout:
+        return 'Timeout; check connection and DNS server.'
+    else:
+        return ', '.join(_data)
 
 
-class Connect():
+def check_ping(node, count='9'):
     """
-    Provides the following methods:
-        ssh() open an ssh channel with a given node and command processing.
-        telnet() open a telnet chanel with a given host and process commands.
+    Pings node (IP address or hostname) with a load of 1378 bytes, with a given
+    number of times (9 by default), at a 200ms interval. Returns a ping
+    statistics as a string otherwise an exception.
+
+    Example:
+        import toolkit
+        print(toolkit.check_ping('google.com', '4'))
+
+    :param node: string, IP address or hostname in DNS.
+    :param count: string, number of echo requests.
+    :return: string, containing ping related information or an exception.
     """
-
-    def __init__(self, node, commands, username=None, password=None):
-        self.node = node
-        self.commands = commands
-        self.username = username
-        self.password = password
-
-    def ssh(self):
-        """Open an SSH channel with a given node (IP or hostname) and process
-        command(s) and its output. Takes a node, username, password, and a list
-        object containing the command(s)."""
-        paramiko.util.log_to_file('paramiko.log')
-        client = paramiko.SSHClient()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        try:
-            client.connect(self.node, 22, self.username, self.password)
-        except socket.error:
-            return 'Node %s is not answering; check hostname or IP.' % self.node
-        except paramiko.AuthenticationException:
-            return 'Authentication failed; check username and password.'
-        except KeyboardInterrupt:
-            print '  Goodbye'
-            exit()
-        else:
-            for command in self.commands:
-                stdin, stdout, stderr = client.exec_command(command)
-                print stdout.read()
+    _interval = '.2'  # 200ms
+    _size = '1350'    # bytes
+    _command = 'ping -c {} -i {} -s {} {} | grep -1 loss'.format(
+        count, _interval, _size, node)
+    try:
+        output = subprocess.check_output(_command, shell=True).decode('utf-8').strip()
+    except subprocess.CalledProcessError as e:
+        return str(e)
+    else:
+        return output
 
 
-    def telnet(self):
-        """Open a telnet channel with a given node (IP or hostname) and process
-        command(s) and its output. Takes a node, username, password, and a list
-        object containing the command(s)."""
+def check_socket(node, port, kind='tcp'):
+    """
+    Check the status of a given socket using node, port, and kind as arguments.
+    Returns 'open' is the socket is open otherwise an exception.
 
-        try:
-            tn = telnetlib.Telnet(self.node)
-            tn.read_until('login: ')
-            tn.write(self.username + '\n')
-            tn.read_until('Password: ')
-            tn.write(self.password + '\n')
-        except socket.error as e:
-            print 'Unable to connect to %s. %s' % (self.node, e)
-        except EOFError as e:
-            print 'Connection closed and no data from %s. %s' % (self.node, e)
-        except KeyboardInterrupt:
-            print '  Goodbye'
-            exit()
-        else:
-            for command in self.commands:
-                tn.write(command + '\n')
-                return tn.read_all()
-        finally:
-            tn.close()
+    Example:
+        import toolkit
+        print(toolkit.check_socket('yahoo.com', 80, 'tcp'))
+
+    :param node: string, IP address or hostname in DNS.
+    :param port: integer, TCP/UDP port number.
+    :param kind: string, port type TCP or UDP.
+    :return: string, open otherwise and exception.
+    """
+    if kind.lower() == 'udp':
+        _sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    else:
+        _sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        _sock.connect((node, port))
+        _sock.close()
+    except socket.error as e:
+        return str(e)
+    except OverflowError as e:
+        return str(e)
+    else:
+        return 'open'
+
+
+def check_url(url):
+    """
+    Check the status of a given URL and returns the HTTP code as a string.
+    Returns the HTTP status code otherwise an exception.
+
+    Example:
+        import toolkit
+        print(toolkit.check_url('https://yahoo.com'))
+
+    :param url: string, URL/URI to check.
+    :return: string, HTTP code otherwise and exception.
+    """
+    try:
+        _connection = urllib.request.urlopen(url)
+    except urllib.error.URLError as e:
+        return str(e)
+    except ValueError as e:
+        return str(e)
+    except ConnectionResetError as e:
+        return str(e)
+    else:
+        return str(_connection.getcode())
+
+
+def main():
+    hostname = 'yahoo.com'
+    print(check_dns(hostname, ['8.8.8.8'], 'a'))
+    print(check_ping(hostname, '3'))
+    print(check_socket(hostname, 80, 'tcp'))
+    print(check_url('https://yahoo.com'))
+
+if __name__ == "__main__":
+    main()
